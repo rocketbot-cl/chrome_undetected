ced3f1543d77fafa0718a594085f789f93362262
nico-garcia-r <nicolas.garcia@rocketbot.com>
2024-12-10 12:37:19 -0300

    feat: add .gitignore file to ignore default profile directory fix(__init__.py): refactor import statement for psutil for clarity feat(__init__.py): add port parameter to UndetectedBrowser initialization feat(__init__.py): implement wait_for_object method to wait for specific conditions feat(undetectedObject.py): add wait_for_object method to handle element conditions chore(package.json): update version to 1.1.1 and add wait_for_object module details

--- libs/undetectedObject.py
+++ libs/undetectedObject.py
@@ -1,6 +1,21 @@
 import subprocess
 import undetected_chromedriver_uc as uc
 from webdriver_manager_uc.chrome import ChromeDriverManager
+from selenium_uc.webdriver.common.by import By
+from selenium_uc.webdriver.support.ui import WebDriverWait
+from selenium_uc.webdriver.support import expected_conditions as EC
+from selenium_uc.common.exceptions import TimeoutException
+
+BY_TYPES = {
+    "id": By.ID,
+    "class": By.CLASS_NAME,
+    "xpath": By.XPATH,
+    "css": By.CSS_SELECTOR,
+    "link": By.LINK_TEXT,
+    "partial_link": By.PARTIAL_LINK_TEXT,
+    "name": By.NAME,
+    "tag": By.TAG_NAME
+}
 
 
 class UndetectedBrowser:
@@ -9,6 +24,12 @@ class UndetectedBrowser:
         self.profile_path = folderPath if folderPath else " "
         self.browser_path = 'start "" chrome'
         self.port = port
+
+    def get_driver_class(self):
+        if self.driver:
+            return self.driver.__class__
+        else:
+            return None
 
     def launch_browser(self):
         subprocess.Popen(" ".join([self.browser_path, "--remote-debugging-port="+self.port, "--user-data-dir=" + self.profile_path + ""]), shell=True)
@@ -25,6 +46,28 @@ class UndetectedBrowser:
         self.driver = uc.Chrome(options=options, browser_executable_path=driver_exe_path, executable_path=driver_exe_path, driver_executable_path=driver_exe_path)
 
         return self.driver
+
+    def wait_for_object(self, data, data_type, condition="clickable", wait_max=10):
+        try:
+            wait = WebDriverWait(self.driver, wait_max)
+            if condition == "clickable":
+                wait.until(EC.element_to_be_clickable((BY_TYPES[data_type], data)))
+            elif condition == "visible":
+                wait.until(EC.visibility_of_element_located((BY_TYPES[data_type], data)))
+            elif condition == "not_visible":
+                wait.until_not(EC.visibility_of_element_located((BY_TYPES[data_type], data)))
+            elif condition == "present":
+                wait.until(EC.presence_of_element_located((BY_TYPES[data_type], data)))
+            else:
+                raise Exception("Invalid condition type")
+
+            return True
+
+        except TimeoutException:
+            return False
+
+        except Exception as e:
+            raise e
 
 
 
